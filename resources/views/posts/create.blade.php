30e59175d8371f259dc50b2eb3d9952607c2be1a
José Contreras <mexidios.mx@gmail.com>
2024-04-27 11:49:21 -0600

    feat: Crea formulario y ruta para subida de imágenes

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,10 +4,22 @@
     Crea un post
 @endsection
 
+@push('styles')
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+@endpush
+
 @section('contenido')
     <div class="md:flex md:items-center">
         <div class="md:w-1/2 px-10">
-            imagen aqui
+            <form
+                action="{{ route('imagenes.store') }}"
+                method="POST"
+                enctype="multipart/form-data"
+                id="dropzone"
+                class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center"
+            >
+                @csrf
+            </form>
         </div>
         <div class="md:w-1/2 px-10 bg-white p-6 rounded-lg shadow-xl">
             <form action="{{ route('register') }}" method="POST">
