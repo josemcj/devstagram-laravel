e99ecaee76c473044a7777741994370dda8eec4d
José Contreras <mexidios.mx@gmail.com>
2024-05-02 13:13:29 -0600

    feat: Guarda imagen en el servidor

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,15 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
     public function store(Request $request)
     {
         // $input = $request->all();
-        $imagen = $request->file('file');
+        $image = $request->file('file');
+        $image_name = Str::uuid() . '.' . $image->extension();
 
-        return response()->json([ 'imagen' => $imagen->extension() ]);
+        $image_server = Image::make($image);
+        // Ajustar tamaño de la imagen
+        $image_server->fit(1000, 1000);
+
+        // Crear el path de la imagen
+        $image_path = public_path('uploads') . '/' . $image_name;
+
+        // Subir imagen al servidor
+        $image_server->save($image_path);
+
+        return response()->json(['imagen' => $image_name]);
     }
 }
