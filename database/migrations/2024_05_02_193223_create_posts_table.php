600cac4890d49aaf9746661ce3cb8577c926cf7e
José Contreras <mexidios.mx@gmail.com>
2025-01-30 17:35:58 -0600

    refactor: change table language

--- database/migrations/2024_05_02_193223_create_posts_table.php
+++ database/migrations/2024_05_02_193223_create_posts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,9 +12,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('titulo');
-            $table->text('descripcion');
-            $table->string('imagen');
+            $table->string('title');
+            $table->text('description');
+            $table->string('image');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
