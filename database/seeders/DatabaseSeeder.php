db1b3f620ca30ebaa1aa67850ff92a6c1c41268f
José Contreras <mexidios.mx@gmail.com>
2025-02-04 09:53:02 -0600

    chore: adds 'use' import

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,13 +14,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
             'username' => 'test',
             'password' => '123456'
         ]);
 
-        \App\Models\User::factory(2)->create();
+        User::factory(2)->create();
     }
 }
